889c83eeacaa4ded5973f54f18ee0b5656e9ee3e
Brian Martin <Brian Martin>
2017-08-02 09:52:58 -0400

    variables created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+	var correctAnswer = 0;
+	var wrongAnswer = 0;
+	var userAnswer = [];
+	var answerKey = {
+		questionOne: "answer1",
+		questionTwo: "answer2",
+		questionThree: "answer 3",
+		questionFour: "answer 4",
+
+	
+
+	};//answerKey object
 
 
 }); //document.ready function
