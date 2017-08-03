f0ee1d9a6b5bdb9d255f7d8753398dbc09af4a36
Brian Martin <Brian Martin>
2017-08-02 23:00:37 -0400

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,28 @@ $(document).ready(function() {
 	var userAnswer = [];
 	var question = ["What is the capital of New York?", "What is the capital of California?", "What is the capital of Florida?", "What is the capital of Texas?"];
 	var answerKey = ["Albany", "Sacramento", "Tallahassee", "Austin"];
+	var timer = 30;
+	var intervalId;
 
 
+	//Start Timer
+    function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
 
-}); //document.ready function
+    function decrement() {
+      timer--;
+      $("#clock").html("<h2>Time Remaining: " + timer + "</h2>");
+      if (timer === 0) {
+        stop();
+        $("#question").html("Time's Up!");
+      }
+    }
+
+	//Stop Timer
+    function stop() {
+		clearInterval(intervalId);
+    }
+
+});//document.ready
+
