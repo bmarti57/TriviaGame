859d80b5ed6f1f39546bc76f7afc7559d79df058
Brian Martin <Brian Martin>
2017-08-02 23:14:44 -0400

    next page timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	var question = ["What is the capital of New York?", "What is the capital of California?", "What is the capital of Florida?", "What is the capital of Texas?"];
 	var answerKey = ["Albany", "Sacramento", "Tallahassee", "Austin"];
 	var timer = 30;
+	var reset = 5;
 	var intervalId;
 
 
@@ -21,13 +22,36 @@ $(document).ready(function() {
       if (timer === 0) {
         stop();
         $("#question").html("Time's Up!");
+        //wrongAnswer();
       }
+    }
+
+    function nextPage() {
+    	intervalId = setInterval(countDown, 1000);
+    }
+
+    function countDown() {
+    	reset--;
+    	$("#clock").html("<h2>Next Question in " + reset + " seconds</p>");
+    	if (reset === 0) {
+    		stop();
+    	}
     }
 
 	//Stop Timer
     function stop() {
 		clearInterval(intervalId);
     }
+
+    function rightWrong() {
+    	if (userAnswer === answerKey[i]) {
+    		correctAnswer++;
+    		userAnswer = [];
+
+    	}
+    }
+
+
 
 });//document.ready
 
